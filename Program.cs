7019ce55015c7fe770eae3e68f7453b06253199a
Aravindh Oruganti <138811150+aravindhrao24@users.noreply.github.com>
2025-01-28 00:48:30 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 8;
+            int total = num1 + num2;
+            Console.WriteLine(total);
+            int count = 10;
+            count += 5;
+            Console.WriteLine(count);
+        }
+    }
+}
